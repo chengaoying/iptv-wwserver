08983f45b076b46a8710cb8793aae07d84184ddd
maqian <maqian.cn@gmail.com>
2012-08-30 15:23:46 +0800

    add RolePropsMapper.xml

--- src/main/java/cn/ohyeah/ww/dao/IGameRoleDao.java
+++ src/main/java/cn/ohyeah/ww/dao/IGameRoleDao.java
@@ -1,12 +1,14 @@
 package cn.ohyeah.ww.dao;
 
 import cn.ohyeah.ww.model.GameRole;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 public interface IGameRoleDao {
     public void create(GameRole role);
     public int update(GameRole role);
+    @Cacheable("gameRole")
     public GameRole read(int roleId);
     public GameRole readByName(String roleName);
     public List<GameRole> readRoles(String userId);
