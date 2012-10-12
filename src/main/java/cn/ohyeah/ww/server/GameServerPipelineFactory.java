7e36a9361293cafd328c1bb0eca54c6e83a8b0d6
maqian <maqian.cn@gmail.com>
2012-10-12 15:35:04 +0800

    update

--- src/main/java/cn/ohyeah/ww/server/GameServerPipelineFactory.java
+++ src/main/java/cn/ohyeah/ww/server/GameServerPipelineFactory.java
@@ -3,12 +3,13 @@ package cn.ohyeah.ww.server;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.handler.execution.ExecutionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameServerPipelineFactory implements ChannelPipelineFactory {
-    //private ExecutionHandler executionHandler;
+    private ExecutionHandler executionHandler;
     private RequestHandler requestHandler;
 
     @Autowired
@@ -20,7 +21,7 @@ public class GameServerPipelineFactory implements ChannelPipelineFactory {
     public ChannelPipeline getPipeline() throws Exception {
         ChannelPipeline pipeline = Channels.pipeline(
                 new RequestDecoder(16*1024, 4, 4),
-                //executionHandler,
+                executionHandler,
                 requestHandler
         );
         return pipeline;
